24ce19b228c87377fe545a06b8b4ebc3e6c4b68a
Hykoo13 <haikmonossiandev@gmail.com>
2026-03-03 11:19:49 +0100

    job1

---
+++ jour2/job1.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Haik2004.",
+    database="laplateforme"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM etudiants")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
